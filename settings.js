c09e2c9215f0a849c9dcdbe9dec597728385d978
RennStore123 <110625568+RennStore123@users.noreply.github.com>
2022-08-23 09:56:37 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,9 +13,8 @@ global.APIKeys = {
 }
 
 // setting 
-global.autoread = false // auto read message
+global.autoread = false // auto read pesan / message
 global.autobio = true 
-global.userRegister = true 
 
 //language
 //Available in indonesia , english & spanyol 
@@ -25,45 +24,30 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Renn" //namabot kalian
-global.ownername= "RennStore" //nama kalian
+global.botname = "Renn-md" //namabot kalian
+global.ownername= "Renn-md" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.github = "https://zeeone-ofc.github.io/" //bebas
+global.email = "derentino99@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
 global.premium = ['6282184976717'] //premium user
-global.owner = ["6282184976717","18552042201"] //ganti agar fitur owner bisa di gunakan
+global.owner = ["6282184976717","6282184976717"] //ganti agar fitur owner bisa di gunakan
 global.ownernomer = "6282184976717" // nomor wa kalian
-global.ownernomerr = "+18552042201" //nmr wa kalian
-global.thumbnail = "./image/img_20220805_013451570.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
+global.ownernomerr = "+6282184976717" //nmr wa kalian
+global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.packname = '© Renn' //sticker wm ubah
-global.author = 'Renn Bot' //sticker wm ganti nama kalian
+global.packname = 'Renn-md' //sticker wm ubah
+global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
-global.multiplier = 69
+
 global.limitawal = {
     premium: "Infinity",
     free: 100
 }
-
-global.adventureRPG = 'https://telegra.ph/file/30bef343159005b622ced.jpg'
-global.bankRPG = 'https://telegra.ph/file/1ee3e007de22f6add4a31.jpg'
-global.bansosRPG = 'https://telegra.ph/file/646af753f38fbf7bbbccc.jpg'
-global.bansosRPG_ = 'https://telegra.ph/file/ba18a18aac022170c8056.jpg'
-global.berburuRPG = 'https://telegra.ph/file/95a202f631ab7172c3f25.jpg'
-global.inventoryRPG = 'https://telegra.ph/file/82b21932692f82fce8664.jpg'
-global.cooldownRPG = 'https://telegra.ph/file/5e1e191a47028d9648ffa.jpg'
-global.dailyRPG = 'https://telegra.ph/file/17e857848a3afdb1c4fd8.jpg'
-global.hourlyRPG = 'https://telegra.ph/file/bcabf71c0f56207d1629e.jpg'
-global.kandangRPG = 'https://telegra.ph/file/89c3b3ec69d373da8f64b.jpg'
-global.kolamRGP = 'https://telegra.ph/file/0bc07117c78b2c9674b7d.jpg'
-global.mancingRPG = 'https://telegra.ph/file/1420d26b7f04f84993225.jpg'
-global.monthlyRPG = 'https://telegra.ph/file/7eadf65b95d9f7a3ecb46.jpg'
-global.weeklyRPG = 'https://telegra.ph/file/ffc85895d14db97931f0f.jpg'
 
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
